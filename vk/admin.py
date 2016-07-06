613bfe0a9eaebc9df7930f8333b32157abedd24b
Vitaly Serov <serovvitaly@gmail.com>
2016-07-07 00:21:10 +0300

    работа над админкой

--- vk/admin.py
+++ vk/admin.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
 from django.db import models
+from django.contrib.contenttypes.admin import GenericForeignKey
 
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
+    domain = models.CharField(max_length=100)
     description = models.TextField()
 
 
+def receive_posts(modeladmin, request, queryset):
+    #queryset.update(status='p')
+    for group in queryset:
+        print(group.id)
+    print("Получние записей со стены")
+receive_posts.short_description = "Получить записи со стены"
+
+
+class Image(models.Model):
+    group = models.ForeignKey(Group)
+
+
+class ImageInline(GenericForeignKey):
+    model = Image
+
+
 class GroupAdmin(admin.ModelAdmin):
-    fields = ('name', 'description')
-    list_display = ('name', 'description')
+    fields = ('name', 'domain', 'description')
+    list_display = ('name', 'domain', 'description')
+    empty_value_display = '-empty-'
+    actions = [receive_posts]
+    inlines = [
+        ImageInline,
+    ]
+
 
 admin.site.register(Group, GroupAdmin)
